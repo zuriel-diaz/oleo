9abf6f6f43ec4a2934456389744004321dd3b827
xthr3mx <xthr3mx@gmail.com>
2016-06-21 08:57:03 -0500

    REST Endpoint added.

--- main.py
+++ main.py
@@ -12,6 +12,7 @@
 import time
 import pyupm_grove as grove
 import pyupm_i2clcd as lcd
+import requests
 
 # Initialize Jhd1313m1 at 0x3E (LCD_ADDRESS) and 0x62 (RGB_ADDRESS)
 myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
@@ -24,6 +25,9 @@ myLcd.setColor(53, 39, 249)
 button_pb = grove.GroveButton(2)
 button_touch = grove.GroveButton(3)
 
+# Endpoint
+URL = 'http://52.53.244.233:3060/v1/challenges?pretty=true'
+
 def getMessage(type_button):
 	messages = {
 		'pb': 'Yes',
@@ -34,6 +38,8 @@ def getMessage(type_button):
 def sendNotification(type_button):
 	message = getMessage(type_button)
 	myLcd.clear()
+	# print message
+	print requests.get(URL, None)
 	myLcd.write(message)
 	time.sleep(1) 
 	return
