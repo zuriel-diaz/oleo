a77f75fb56907874eb1db431287dc22996e44cc7
xthr3mx <xthr3mx@gmail.com>
2016-06-20 22:56:11 -0500

    adding message (LCD).

--- main.py
+++ main.py
@@ -15,13 +15,26 @@ import pyupm_i2clcd as lcd
 
 # Initialize Jhd1313m1 at 0x3E (LCD_ADDRESS) and 0x62 (RGB_ADDRESS)
 myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
+myLcd.setCursor(0,0)
+# RGB Red
+#myLcd.setColor(255, 0, 0)
+# RGB Blue
+myLcd.setColor(53, 39, 249)
 
 # Initialize buttons using GPIO pin [2,3]
 button_pb = grove.GroveButton(2)
 button_touch = grove.GroveButton(3)
 
+def getMessage(type_button):
+	messages = {
+		'pb': 'Push button',
+		'touch': 'Touch'
+	}
+	return messages.get(type_button)
+
 def sendNotification(type_button):
-	print "test"
+	message = getMessage(type_button)
+	myLcd.write(message)
 	time.sleep(1) 
 	return
 
